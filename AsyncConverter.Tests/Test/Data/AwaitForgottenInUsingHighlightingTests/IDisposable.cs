eb4797dad18c72e58254b6fa99434ef9783c75be
i.mamay <i.mamay@skbkontur.ru>
2017-06-22 21:16:28 +0500

    fist tests

---
+++ AsyncConverter.Tests/Test/Data/AwaitForgottenInUsingHighlightingTests/IDisposable.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+
+namespace AsyncConverter.Tests.Test.Data.FixReturnValueToTaskTests
+{
+    public class Class
+    {
+        public async Task TestAsync()
+        {
+            using (Method2())
+            {
+
+            }
+        }
+
+        private Task<IDisposable> Method2()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
